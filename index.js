c23dbdb9bba4e88129a20a8b9e99dc041ee8e087
tawneejoneskell <natawneetjk@gmail.com>
2022-10-05 19:32:51 -0400

    to do project update

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const ulList=document.getElementById("toDoList");
 const inputAdd=document.getElementById("addItem");
+const removeToDo=document.getElementById("removeButton")
 const addItemButton=document.getElementById("submitButton");
 
 const addToList=()=>{
     const inputText=inputAdd.value;
     const newItem=document.createElement('li');
-    newItem.innerHTML=`<input type="radio"><input type="checkbox">${inputText}`;
+    newItem.innerHTML=`<input type="checkbox" class='itemRemove'>${inputText}`;
     ulList.appendChild(newItem);
-
 }
+const removeFromList=()=>{
+  const removeListItems=document.getElementsByClassName('itemRemove');
+  for(let i=0;i<removeListItems.length; i++){
+    console.log(removeListItems[i].checked);
+    if(removeListItems[i].checked){
+    ulList.removeChild(ulList.children[i]);
+    }
+}
+}
+
 
